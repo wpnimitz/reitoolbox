06de628b066f81635d190d2fba23db196d441cc6
wpnimitz <DefendER6489>
2020-03-06 23:21:15 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -55,7 +55,7 @@ function generate_new_rei_style() {
 add_filter( 'gform_submit_button', 'form_submit_button', 10, 2 );
 function form_submit_button( $button, $form ) {
 	$id = 'gform_submit_button_' . $form['id'];
-	$text = $form["button"]["text"]
+	$text = $form["button"]["text"];
     return '<div class="et_pb_button_module_wrapper et_pb_button_0_wrapper et_pb_button_alignment_center et_pb_module ">
 				<button class="et_pb_button et_pb_button_0 et_pb_bg_layout_light" id="'.$id.'">'.$text.'</button>
 			</div>';
